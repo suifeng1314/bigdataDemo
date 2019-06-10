e6f0cf39e6e9aed8b91e07de3113ef6c02f11ccd
longtao <844253749@qq.com>
2019-06-10 09:44:38 +0800

    111

---
+++ src/main/java/com/longtao/Test.java
@@ -0,0 +1,16 @@
+package com.longtao;
+
+
+/**
+ * Copyright (C), 2006-2010, ChengDu ybyainfo. Co., Ltd.
+ * FileName: Test.java
+ *
+ * @version 1.00
+ * @Author lt
+ * @Date 2019/6/10 9:40
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
