783f9dce9d5f632d120e06e216b97d50e7dd3ca3
LongTao <844253749@qq.com>
2019-06-15 21:26:20 +0800

    压缩方式

--- src/main/java/com/longtao/wordcount/WordCountDriver.java
+++ src/main/java/com/longtao/wordcount/WordCountDriver.java
@@ -4,6 +4,9 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.BZip2Codec;
+import org.apache.hadoop.io.compress.DefaultCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -27,6 +30,12 @@ public class WordCountDriver {
         // 6.设置reducer阶段输出的数据类型
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+
+        // 设置reduce端输出压缩
+        FileOutputFormat.setCompressOutput(job,true);// 是否压缩
+        // 压缩方式
+//        FileOutputFormat.setOutputCompressorClass(job,BZip2Codec.class);
+        FileOutputFormat.setOutputCompressorClass(job,DefaultCodec.class);
         // 7.设置数据输入的路径
         FileInputFormat.setInputPaths(job,new Path(args[0]));
         // 8.设置数据输出的路径
