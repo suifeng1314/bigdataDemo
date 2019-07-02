31a36b8b748af8227ab56178571e52857ada3da5
LongTao <844253749@qq.com>
2019-07-03 01:02:47 +0800

    hbase 操作api 增删改查

--- src/main/java/com/longtao/hbase/HbaseTest.java
+++ src/main/java/com/longtao/hbase/HbaseTest.java
@@ -28,11 +28,29 @@ public class HbaseTest {
         return flag;
     }
 
+    // 创建表
+    public static void createTable(String tableName,String... columnFamilly) throws IOException {
+        // 1.管理器
+        Connection connection = ConnectionFactory.createConnection(conf);
+
+        HBaseAdmin admin = (HBaseAdmin) connection.getAdmin();
+        // 2.创建描述器
+        HTableDescriptor hTableDescriptor = new HTableDescriptor(TableName.valueOf(tableName));
+        // 3.指定多个列簇
+        for(String cf: columnFamilly){
+            hTableDescriptor.addFamily(new HColumnDescriptor(cf));
+        }
+        // 4.创建表
+        admin.createTable(hTableDescriptor);
+        System.out.println("创建表成功！");
+
+    }
 
     public static void main(String[] args) throws IOException {
-        System.out.println(1);
-        boolean flag = isExist("student1");
-        System.out.println("表是否存在：" + flag);
+        System.out.println("main~~~~~~~~");
+//        boolean flag = isExist("student1");
+//        System.out.println("表是否存在：" + flag);
+        createTable("teacher","info1","info2","info3");
 
     }
 
